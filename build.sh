8f32cbaaecf14330d90463671f36e1dcddae4c5a
Toby Rumans <toby@plink.com>
2015-03-01 15:20:56 -0800

    Temporarily remove jasmine from build until I have JS tests

--- build.sh
+++ build.sh
@@ -5,11 +5,11 @@ export RAILS_ENV=test
 set -e
 
 echo 'Running test setup'
-bundle exec rake rake db:test:prepare
+bundle exec rake db:test:prepare
 
 
 echo 'Running Jasmine'
-bundle exec rake jasmine:ci
+# bundle exec rake jasmine:ci
 
 echo 'Running rspec'
 bundle exec rspec spec
